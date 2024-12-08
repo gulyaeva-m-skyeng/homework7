70d8c5cfb98fcc264611f5c2c343e548e22c6a33
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-12-08 23:23:28 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//task 1
+        System.out.println("Task 1");
+        printIsLeapYear(2021);
+    }
+
+    public static void printIsLeapYear(int year) {
+        boolean yearIsLeap = isLeap(year);
+        printIsLeapYearResult(year, yearIsLeap);
+    }
+
+    private static boolean isLeap(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    private static void printIsLeapYearResult(int year, boolean yearIsLeap) {
+        if (yearIsLeap) {
+            System.out.println(year + " високосный");
+        } else {
+            System.out.println(year + " не является високосным");
+        }
     }
 }
+
