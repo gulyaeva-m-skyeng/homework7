81abc5f928f9660865011b98ef288871045cb3f9
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-12-16 01:21:39 +0300

    Задание 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-
-//task 1
-        System.out.println("Task 1");
         printIsLeapYear(2021);
+        recommendApplicationVersion(0, 2016);
+        calculateDeliveryDays(25);
     }
 
     public static void printIsLeapYear(int year) {
@@ -22,5 +22,50 @@ public class Main {
             System.out.println(year + " не является високосным");
         }
     }
-}
 
+    public static void recommendApplicationVersion(int clientOS, int deviceYear) {
+        boolean deviceIsOld = isDeviceOld(deviceYear);
+        System.out.print("Установите ");
+        if (deviceIsOld) {
+            System.out.print("lite ");
+        }
+        System.out.print("версию для ");
+        if (clientOS == 0) {
+            System.out.println("iOS");
+        } else if (clientOS == 1) {
+            System.out.println("Android");
+        } else {
+            throw new RuntimeException("Неизвестная система");
+        }
+    }
+
+    private static boolean isDeviceOld(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear <= currentYear;
+    }
+
+    public static int calculateDeliveryDays(int deliveryDistance) {
+        int result = 1;
+        if (deliveryDistance > 20) {
+            result++;
+        }
+        if (deliveryDistance > 60) {
+            result++;
+        }
+        if (deliveryDistance > 100) {
+            result = -1;
+        }
+        return result;
+
+        switch (deliveryDistance) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Количество дней для доставки составит " + deliveryDistance);
+                break;
+            case -1:
+                System.out.println("Доставка невозможна");
+                break;
+        }
+    }
+}
